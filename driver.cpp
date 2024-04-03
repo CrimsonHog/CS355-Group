72002059f2f4006c7b6a26838c84ad19579ae194
Ethan Rinke <111534189+CrimsonHog@users.noreply.github.com>
2024-04-03 18:06:36 -0500

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,6 +1,22 @@
+/*
+    Group Members: 
+        Ethan 
+        Gage
+        Lydia 
+        Kelson
+        
+*/
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main(){
+    // Front end, doesnt have to be anything to complicated
+    // This could be where we call and create objects from header files
+            // New variables do not all need to be at the top, call them where necessary
+            // No need to clutter the reading of the driver
     
+    // Create menu Here
+    
+    return 0;
 }
