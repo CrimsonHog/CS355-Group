2de4fb859e5441477cf6efff268a130c4af73b96
Ethan Rinke <111534189+CrimsonHog@users.noreply.github.com>
2024-04-08 18:25:15 -0500

    Add files via upload

--- driver.cpp
+++ driver.cpp
@@ -10,35 +10,61 @@
 #include <cstring>
 using namespace std;
 
+void fileInput(int, string*, int){};
+
+
 int main(){
     // Front end, doesnt have to be anything to complicated
     // This could be where we call and create objects from header files
             // New variables do not all need to be at the top, call them where necessary
             // No need to clutter the reading of the drive
+    
 
+    //Create menu
+
+    // Call Graph calls - creating and taking 
+    //Dummy Graph variables
+    int graphWeight; 
+    string* graphPathway; 
+    int graphData;
+    fileInput(graphWeight, graphPathway, graphData);
+    
+
+    return 0;
+}
+
+// ----------------------------------------------------------
+// Function Name: fileInput
+// Function Type: void 
+// Inputs: Int, string pointer, int
+// output: N/A
+// Description: Function calls graphInfo text file and inputs them into the 
+//              graph node array
+// Author: Ethan
+// ----------------------------------------------------------
+void fileInput(int graphWeight, string* graphPathway, int graphData){
+    
+    
     /*
         Section implemented by: Ethan
     */
     // Dummy Variables for the file 
     // Change once we are able to call the graphNode Header file
-    int weight;
-    string* pathway;
-    int data;
-
+    
     ifstream inputFile;
     inputFile.open("graphInfo.txt");
+
     //File input
     if (inputFile.is_open()){
         while(!inputFile.eof()){
-            inputFile >> weight;
-            inputFile >> *pathway;
-            inputFile >> data;
+            inputFile >> graphWeight;
+            inputFile >> *graphPathway;
+            inputFile >> graphData;
         }
         inputFile.close();
     }
 
-    
-    // Create menu Here
+    // Come back and fix this function so that it works with the graphNode Array
+    // Currently does not work as is
 
-    return 0;
-}
+};
