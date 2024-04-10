91011a8295af51543d041dbcb491f33929f9ae48
Ethan Rinke <111534189+CrimsonHog@users.noreply.github.com>
2024-04-10 18:49:09 -0500

    Add files via upload

--- BinaryHeap.cpp
+++ BinaryHeap.cpp
@@ -1,116 +1,69 @@
+#ifndef _BINARYHEAP_CPP
+#define _BINARYHEAP_CPP
 #include <iostream>
 #include <fstream>
 #include "BinaryHeap.h"
 using namespace std;
-
-Node::Node()
-{
-	for (int i = 0; i < CITY_COUNT; i++)
-	{
-		city[i] = "Default City"; 
-		weight[i] = INT_MAX;
-	}
-}
-
-Node::Node(string c[], int w[]) 
-{
-	ifstream cityAndWeights;
-	cityAndWeights.open("");
-	
-	for (int i = 0; i < CITY_COUNT; i++) // have cities be half the txt file and corresponding weights be other half 
-	{
-		if (i <= (CITY_COUNT / 2))
-		{
-			cityAndWeights >> city[i];
-		}
-		else 
-		{
-			cityAndWeights >> weight[i];
-		}
-	}
-	
-	cityAndWeights.close();
-};
-
 /*
 	Function Name: PercolateUp
-	Function Inputs: Node factor[factorSize], int hole
+	Function Inputs: GraphNode newNode
 	Function Outputs: N/A
 	Function Description: 
-	Author: Ethan
+	Author: Ethan / Kelson
 	Testers:
 */
-void BinaryHeap::PercolateUp(Node factor[factorSize], int hole){
-	hole = factorSize - 1;
-	int parent = hole / 2;
-	Node temp = factor[hole];
-	
-	while (hole > 1 && temp.weight < factor[parent].weight){
-		factor[hole] = factor[parent];
-		hole = parent;
-		parent = hole / 2;
+void BinaryHeap::PercolateUp(GraphNode newNode)
+{
+	largestIndex++;
+	heap[largestIndex] = newNode;
+	int currentIndex = largestIndex;
+	while(currentIndex > 0 && heap[currentIndex].weight < heap[(currentIndex - 1) / 2].weight)
+	{
+		//swap the newNode and parent
+		GraphNode temp = heap[currentIndex];
+		heap[currentIndex] = heap[(currentIndex - 1) / 2];
+		heap[(currentIndex - 1) / 2] = temp;
+		//update the index
+		currentIndex = (currentIndex - 1) / 2;
 	}
-	factor[hole] = temp;
 };
 
 /*
 	Function Name: PercolateDown
-	Function Inputs: Node factor[factorSize], int hole
+	Function Inputs: GraphNode newNode
 	Function Outputs: N/A
 	Function Description: 
-	Author: Ethan
+	Author: Ethan / Kelson
 	Testers:
 */
-void BinaryHeap::PercolateDown(Node factor[factorSize], int hole){
-	int child;
-	Node temp = factor[hole];
+void BinaryHeap::PercolateDown(int index)
+{
 	
-	while (hole * 2 <= factorSize){
-		child = hole * 2;
-		if (child != factorSize && factor[child + 1].weight < factor[child].weight){
-			child++;
-		}
-		if (factor[child].weight < temp.weight){
-			factor[hole] = factor[child];
-		}
-		else{
-			break;
-		}
-		hole = child;
-	}
-	factor[hole] = temp;
-};
+}
 
 /*
 	Function Name: Insert
-	Function Inputs: string c, int w, Node factor[]
+	Function Inputs: 
 	Function Outputs: N/A
 	Function Description: Insert into node array and percolate up to make sure any new node is in the correct position
 	Author: Ethan
 	Testers:
 */
-void Insert(string c, int w, Node factor[]){
-	Node temp;
-	temp.city = c;
-	temp.weight = w;
-	
-	PercolateUp(factor, factorSize);
+void BinaryHeap::Insert(GraphNode newNode){
+	PercolateUp(newNode);
 };
 
 /*
 	Funtion Name: Remove
-	Function Inputs: Node factor[], int val
+	Function Inputs: GraphNode factor[], int val
 	Function Outputs: N/A
 	Function Description: Remove the root node from the array and percolate down to make sure the array is in the correct order
 	Author: Ethan
 	Testers: 
 */
-void Remove(Node factor[], int val){
-	Node temp = factor[1];
-	factor[1] = factor[factorSize];
-	factor[factorSize] = temp;
-	
-	PercolateDown(factor, factorSize);
+void BinaryHeap::Remove(GraphNode newNode){
+	PercolateDown(0);
+	largestIndex--;
 };
 
 /*
@@ -126,3 +79,6 @@ void BinaryHeap::PrintBinaryHeap(Node factor)const {
     cout << "City: " << factor.city[i] << "has Weight: " << factor.weight[i] << endl ;
   }
 }
+
+
+#endif
