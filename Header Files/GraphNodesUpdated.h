3de270b85dc37e520e470c65edc64dec8c45b907
Lydia Sparks <109820487+LydiaSparks@users.noreply.github.com>
2024-04-11 00:14:07 -0500

    Add files via upload

---
+++ Header Files/GraphNodesUpdated.h
@@ -0,0 +1,44 @@
+// ---------------------------------------------------------------------------
+// Name: Gage Mathis, Kelson Moore, Ethan Rinke, Lydia Sparks
+// Course-Section: CS355-Section#02
+// Assignment: Project 4
+// Date due: 
+// Description: This is the header file for the nodes of the graph that is 
+// used with the shortest path algorithm, using the binary heap as a priority
+// queue.
+// ---------------------------------------------------------------------------
+
+#ifndef _GRAPHNODES_H
+#define _GRAPHNODES_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class GraphNodes
+{
+	private:
+		/*
+			* ATTRIBUTES
+		*/
+		int index; // holds the index of the node to help with adjacency matrix
+		string name; // holds the name of the cities(nodes) for convienience of tracking.
+	public:
+		/*
+			* CONSTRUCTORS/METHODS
+		*/
+		//Constructors
+		GraphNodes(); 
+		GraphNodes(string);
+		GraphNodes(int, string);
+		
+		//setters
+		//void ChangeWeight(int);
+		//void ChangeWeight(int, int);
+		//void ChangeName(string);
+		
+		//getters
+		int GetIndex();
+		string GetName();
+};
+
+#endif
