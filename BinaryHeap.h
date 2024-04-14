7b70c42b5b2e86f747e30dd5552ea575dc9dd61a
kmoore3570 <165725062+kmoore3570@users.noreply.github.com>
2024-04-14 17:21:38 -0500

    Added many cout statements for help with debugging

--- BinaryHeap.h
+++ BinaryHeap.h
@@ -1,3 +1,6 @@
+#ifndef _BINARYHEAP_H
+#define _BINARYHEAP_H
+
 // ---------------------------------------------------------------------------
 // Name: Gage Mathis, Kelson Moore, Ethan Rinke, Lydia Sparks
 // Course-Section: CS355-Section#02
@@ -7,37 +10,15 @@
 // used to implement the shortest path algorithm in our road trip project.
 // ---------------------------------------------------------------------------
 
-#ifndef _BINARYHEAP_H
-#define _BINARYHEAP_H
+#include "GraphNode.h"
+#include "Graph.h"
+#include "Edge.h"
 #include <iostream>
+#include <vector>
 using namespace std;
 
 
 //GLOBALS
-const int CITY_COUNT = 20; // number of cities in the txt file
-const int factorSize = 3; // size of the array of Node objects
-
-/*
-	* NODE CLASS
-*/
-class Node
-{
-
-private:
-	/*
-		* ATTRIBUTES
-	*/
-	string city[]; // each city corresponding to txt file
-	int weight[]; // each weight corresponding to txt file
-	
-	/*
-		* METHODS
-	*/
-	Node();
-	Node(string c, int w);
-	
-	friend class BinaryHeap;
-};
 
 /*
 	* BINARY HEAP CLASS
@@ -49,19 +30,21 @@ private:
 	/*
 		* ATTRIBUTES
 	*/
-	
+	vector<GraphNode> heap;
+
 public:
 	/*
 		* METHODS
 	*/
-	void Insert(string c, int w, Node factor[]); // for inserting into the array
-	void Remove(Node factor[], int val); // for removing from the array; only removing the root, so the array is the only parameter
-	bool FindShortestPath(Node factor[]); // finds the shortest path based upon weights; array has weights in Node objects
-	void PercolateUp(Node factor[factorSize], int hole); // used to help with maintaining binary heap ordering property in Insert() and Remove()
-	void PercolateDown(Node factor[factorSize], int hole); // used with BuildHeap()
-	bool BuildHeap(Node factor[]); // sorts the elements of the array into a binary heap
-	int ViewMin(Node factor[]); // allows for the root to be viewed
-	void PrintBinaryHeap(Node factor[])const; // used for testing that the binary heap was created successfully
-};
+	//Constructors
+	BinaryHeap();
+	BinaryHeap(Graph);
+	//keeping order in the heap
+	void PercolateUp(int index); // used to help with maintaining binary heap ordering property in Insert() and Remove()
+	void PercolateDown(int index); // used with BuildHeap()
+	//adding to and taking away from the heap
+	void Insert(GraphNode newNode); // for inserting into the array
+	GraphNode Remove(); // for removing from the array; only removing the root, so the array is the only parameter
+	};
 
 #endif
