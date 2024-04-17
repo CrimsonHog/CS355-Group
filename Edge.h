c606ae0ec4262a8072d475813dd16294e073cf91
kmoore3570 <165725062+kmoore3570@users.noreply.github.com>
2024-04-16 21:51:56 -0500

    Update Edge.h

--- Edge.h
+++ Edge.h
@@ -5,7 +5,7 @@
 // Name: Gage Mathis, Kelson Moore, Ethan Rinke, Lydia Sparks
 // Course-Section: CS355-Section#02
 // Assignment: Project 4
-// Date due: 
+// Date due: 04/16/24
 // Description: This is the header file for the edges of the graph, which will
 // be implemented as an attribute of a GraphNode object. This way, each node
 // will have an edge attribute that shows what the node's edge is connected to, 
@@ -18,17 +18,13 @@ using namespace std;
 class Edge
 {
 private:
-	/*
-		* ATTRIBUTES
-	*/
-	int nodePreviousIndex;
-	int nodeConnectionIndex;
-	int weight;
+	//Attributes
+	int nodePreviousIndex; //Holds the index of the node that comes before the edge
+	int nodeConnectionIndex; //Holds the index of the node that comes after the edge
+	int weight; //The weight of the edge
 	
 public:
-	/*
-		* CONSTRUCTORS/METHODS
-	*/
+	//Constructors
 	Edge();
 	Edge(int, int, int);
 	//setters
@@ -39,7 +35,6 @@ public:
 	int GetPrevConnectionIndex();
 	int GetNodeConnectionIndex();
 	int GetEdgeWeight();
-	
 	//operator overloads
 	void operator=(const Edge&); // allows for a deep copy of data to be performed
 };
