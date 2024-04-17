3698ec0f3bfc7b13c6259459265af4e5588cc736
gagethekidd <122556071+gagethekidd@users.noreply.github.com>
2024-04-16 21:51:32 -0500

    Update BinaryHeap.cpp

--- BinaryHeap.cpp
+++ BinaryHeap.cpp
@@ -1,3 +1,11 @@
+// ---------------------------------------------------------------------------
+// Name: Gage Mathis, Kelson Moore, Ethan Rinke, Lydia Sparks
+// Course-Section: CS355-Section#02
+// Assignment: Project 4
+// Date due: 
+// Description: Implementations of everything from the binary heap header file.
+// ---------------------------------------------------------------------------
+
 #ifndef _BINARYHEAP_CPP
 #define _BINARYHEAP_CPP
 
